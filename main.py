8435dc20d3195e4dde029fb9a853c35616323e71
Cortuzz <oleg.cortuzz1@gmail.com>
2022-04-14 02:44:05 +0700

    Music

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import sys
 import fpstimer
+from pygame import mixer
 
 from converters import *
 
 
 VIDEO_PATH = 'BadApple.mp4'
 
-frame_size = 150
+frame_size = 100
 frame_time = 0.01
+
+
+def play_audio(path):
+    mixer.pre_init(44100, -16, 2, 2048)
+    mixer.init()
+    mixer.music.load(path)
+    mixer.music.set_volume(1)
+    mixer.music.play()
 
 
 def play_video(last_frame):
@@ -22,4 +31,6 @@ def play_video(last_frame):
 
 
 if __name__ == '__main__':
-    play_video(get_ascii(VIDEO_PATH, frame_size))
+    ASCII = get_ascii(VIDEO_PATH, frame_size)
+    play_audio("BadApple.mp3")
+    play_video(ASCII)
